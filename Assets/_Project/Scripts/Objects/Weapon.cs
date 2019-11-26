0de0abe349c2ee5069c968d9975c6f978dc39ecc
Steven Henry <stevennassef97@gmail.com>
2019-11-26 23:19:31 +0200

    1- Created Primary Weapons Scriptable objects

--- Assets/_Project/Scripts/Objects/Weapon.cs
+++ Assets/_Project/Scripts/Objects/Weapon.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 
 
 [System.Serializable]
-[CreateAssetMenu(fileName="Weapon", menuName="Weapons/Basic Weapon")]
+[CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Basic Weapon")]
 public class Weapon : ScriptableObject
 {
 
+    [Header("Weapon Stats")]
     [SerializeField] private string weaponName;
-    [Tooltip("Automatic (fires by holding)/ Single-shot(Fires on singles press).")]
-    [SerializeField] private WeaponFireMode fireMode;
-    [Tooltip("Whether this weapon shots Projectiles or Just uses RayCast, If Projectile type was choosen the weaponProjectile field must be set.")]
-    [SerializeField] private WeaponShootingType shootingType;
-    [Tooltip("The type of projectile the weapon shots, (ONLY used when shootingType is set to Projectile).")]
-    [SerializeField] private WeaponProjectile weaponProjectile;
     [Tooltip("The amount of damage each bullet deals.")]
     [SerializeField] private float damagePerBullet = 5;
     [Tooltip("How many bullets are fired per second.")]
+    [SerializeField] private WeaponFireMode fireMode;
+    [Tooltip("Whether this weapon shots Projectiles or Just uses RayCast, If Projectile type was choosen the weaponProjectile field must be set.")]
     [SerializeField] private float fireRate = 1;
 
     [Tooltip("The maximum amount of bullets a magazine can hold.")]
     [SerializeField] private int ammoCount = 30;
     [Tooltip("The distance the bullets is effective for, after which no damage is dealt.")]
     [SerializeField] private float range = 50;
+    [Tooltip("Automatic (fires by holding)/ Single-shot(Fires on singles press).")]
+    [Header("Projectile")]
+    [SerializeField] private WeaponShootingType shootingType;
+    [Tooltip("The type of projectile the weapon shots, (ONLY used when shootingType is set to Projectile).")]
+    [SerializeField] private WeaponProjectile weaponProjectile;
+
 
     public string WeaponName => weaponName;
     public WeaponFireMode FireMode => fireMode;
