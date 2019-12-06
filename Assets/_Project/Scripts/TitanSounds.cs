92ac90a583ca0a6ea67ef0fb21ccbe2e6085a7ea
Steven Henry <stevennassef97@gmail.com>
2019-12-07 01:07:29 +0200

    Merge branch 'master' of https://github.com/StevenNassef/TitanFall-Replica

---
+++ Assets/_Project/Scripts/TitanSounds.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class TitanSounds : MonoBehaviour
+{
+
+    public string titanName;
+    public GameObject background;
+    public GameObject talk;
+    public GameObject effect;
+
+    private List<AudioClip> welcome;
+    private List<AudioClip> coreReady;
+    private List<AudioClip> coreFire;
+    private List<AudioClip> eject;
+    private List<AudioClip> defence;
+    private List<AudioClip> criticalDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var titanFolder = "Titans/";
+
+        welcome = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/Welcome"));
+        coreReady = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/CoreReady"));
+        coreFire = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/CoreFire"));
+        eject = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/Eject"));
+        defence = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/Defence"));
+        criticalDamage = new List<AudioClip>(Resources.LoadAll<AudioClip>(titanFolder + titanName + "/CriticalDamage"));
+    }
+
+    [ButtonMethod]
+    void playWelcome() {
+        talk.GetComponent<AudioSource>().clip = welcome.GetRandom();
+        talk.GetComponent<AudioSource>().Play();
+    }
+    
+    [ButtonMethod]
+    void playEject() {
+        talk.GetComponent<AudioSource>().clip = eject.GetRandom();
+        talk.GetComponent<AudioSource>().Play();
+    }
+    
+    [ButtonMethod]
+    void playCriticalDamage() {
+        talk.GetComponent<AudioSource>().clip = criticalDamage.GetRandom();
+        talk.GetComponent<AudioSource>().Play();
+    }
+    
+    [ButtonMethod]
+    void playDefence() {
+        effect.GetComponent<AudioSource>().clip = defence.GetRandom();
+        effect.GetComponent<AudioSource>().Play();
+    }
+    
+    [ButtonMethod]
+    void playCoreReady() {
+        talk.GetComponent<AudioSource>().clip = coreReady.GetRandom();
+        talk.GetComponent<AudioSource>().Play();
+    }
+    
+    [ButtonMethod]
+    void playCoreFire() {
+        talk.GetComponent<AudioSource>().clip = coreFire.GetRandom();
+        talk.GetComponent<AudioSource>().Play();
+    }
+
+}
