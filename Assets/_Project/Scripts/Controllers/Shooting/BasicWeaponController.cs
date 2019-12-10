ea9efdd55d7bd2a31279e6f57dc23137c9b1dba9
Steven Henry <stevennassef97@gmail.com>
2019-12-10 20:52:06 +0200

    1- Trying To Fix Weapon Animation

--- Assets/_Project/Scripts/Controllers/Shooting/BasicWeaponController.cs
+++ Assets/_Project/Scripts/Controllers/Shooting/BasicWeaponController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using MyBox;
 [RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Animator))]
 public class BasicWeaponController : MonoBehaviour
@@ -17,7 +18,7 @@ public class BasicWeaponController : MonoBehaviour
     protected WeaponState currentState;
     protected WeaponState previousState;
     protected int currentAmmo;
-    protected bool fire, fireLock, fireWait, reload, reloadLock, currentlyAiming, previouslyAiming;
+    protected bool fire, fireLock, fireWait, reload, reloadLock, currentlyAiming, previouslyAiming, aimLock;
     protected WeaponHolderController weaponHolder;
     protected float timeBetweenBullets = 1;
     protected Vector3 screenCenter;
@@ -82,11 +83,16 @@ public class BasicWeaponController : MonoBehaviour
             currentState = WeaponState.Idle;
             StartCoroutine(AutomaticFire());
         }
+        if (aimLock)
+        {
+            currentlyAiming = false;
+        }
 
         if (currentlyAiming != previouslyAiming)
         {
             weaponAnimator.SetBool("Aim", currentlyAiming);
         }
+
     }
     void Update()
     {
@@ -109,6 +115,7 @@ public class BasicWeaponController : MonoBehaviour
     {
         // Debug.Log("Fire!");
         reloadLock = false;
+        aimLock = false;
         currentAmmo--;
         weaponAnimator.SetTrigger("Shoot");
 
@@ -161,6 +168,7 @@ public class BasicWeaponController : MonoBehaviour
     protected virtual void Reload()
     {
         reloadLock = true;
+        aimLock = true;
         currentState = WeaponState.Reloading;
         weaponAnimator.SetTrigger("Reload");
     }
@@ -169,17 +177,18 @@ public class BasicWeaponController : MonoBehaviour
         currentState = WeaponState.Idle;
         currentAmmo = weapon.AmmoCount;
         reloadLock = false;
+        aimLock = false;
     }
 
     protected void OpenScope()
     {
-        if(scope != null)
+        if (scope != null)
             scope.SetActive(true);
     }
 
     protected void CloseScope()
     {
-        if(scope != null)
+        if (scope != null)
             scope.SetActive(false);
     }
     private IEnumerator AutomaticFire()
@@ -213,6 +222,7 @@ public class BasicWeaponController : MonoBehaviour
     protected void OnEnable()
     {
         CloseScope();
+        aimLock = false;
         if (fireWait)
         {
             float delta = Time.timeSinceLevelLoad - lastDisableTime;
@@ -226,7 +236,16 @@ public class BasicWeaponController : MonoBehaviour
             }
         }
     }
-
+    
+    public void UnequipWeapon()
+    {
+        if(weaponAnimator != null)
+            weaponAnimator.SetTrigger("Unequip");
+    }
+    protected void DisableWeapon()
+    {
+        gameObject.SetActive(false);
+    }
     protected void OnDisable()
     {
         CloseScope();
