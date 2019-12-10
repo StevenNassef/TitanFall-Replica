ba1f46b77493b583cd5678bd9c43587cba012e91
Basem Rizk <basem.rizk@outlook.com>
2019-12-10 12:20:49 +0200

    Prototyping pause screen

---
+++ Assets/_Project/Scripts/UIControllers/PauseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+	public void OnClickMainMenu() {
+        // UnityEngine.SceneManagement.SceneManager.LoadScene("Mainmenu");
+    }
+
+    public void OnClickRestart() {
+		// TODO
+    }
+
+	public void onClickResume() {
+		// TODO
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
