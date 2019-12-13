583f8f2aa0aa9aedbcf95b638b17957ec2c57341
Steven Henry <stevennassef97@gmail.com>
2019-12-13 02:08:48 +0200

    1- Player_Titan Update

---
+++ Assets/_Project/Scripts/Controllers/TitanEmbarkHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitanEmbarkHandler : MonoBehaviour
+{
+    [SerializeField] private float embarkRadius;
+    [SerializeField] private KeyCode embarkKey;
+    [SerializeField] private LayerMask detectionMask;
+    bool embarkLock = false;
+
+    private void OnEnable()
+    {
+        embarkLock = false;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void EmbarkTitan()
+    {
+
+    }
+}
