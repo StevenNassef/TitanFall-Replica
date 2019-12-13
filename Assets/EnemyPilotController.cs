b66ac5fbe90d1ab91b57dd3accf9d6b9a7464d1d
unknown <ibram.medhat2@gmail.com>
2019-12-13 12:57:18 +0200

    1-Adding additional animations and script Folder 2-Applying patrol walking animation in navmesh scene

---
+++ Assets/EnemyPilotController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+public class EnemyPilotController : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public ThirdPersonCharact character; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance > agent.stoppingDistance)
+            character.Move(agent.desiredVelocity, false, false);
+        else
+            character.Move(Vector3.zero, false, false);
+        
+    }
+}
