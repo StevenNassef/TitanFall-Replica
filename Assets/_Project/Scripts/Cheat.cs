6cd57c2fe6d1594aa919187f3db03f9ad3cfe335
mike13k <mikegeorge2012@gmail.com>
2019-12-14 02:17:34 +0200

    update cheats

--- Assets/_Project/Scripts/Cheat.cs
+++ Assets/_Project/Scripts/Cheat.cs
@@ -21,5 +21,18 @@ public class Cheat : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.F11)){
             SceneManager.LoadScene("Prototyping_Sounds");
         }
+
+        if(Input.GetKeyDown(KeyCode.F12)){
+            SceneManager.LoadScene("GameoverScene");
+        }
+
+        if(Input.GetKeyDown(KeyCode.F9)){
+            SceneManager.LoadScene("Prototyping_PauseScene");
+        }
+
+        if(Input.GetKeyDown(KeyCode.F8)){
+            SceneManager.LoadScene("Prototyping_GameMechanics");
+        }
+        
     }
 }
